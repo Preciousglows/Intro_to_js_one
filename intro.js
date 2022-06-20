2b7942a4ad189c9c18eff5e8139f5bba5733a39a
Precious Worukwo <95619663+Preciousglows@users.noreply.github.com>
2022-06-20 23:47:28 +0100

    Add files via upload

---
+++ intro.js
@@ -0,0 +1,8 @@
+
+
+let strOne = "Name: Precious Worukwo";
+let strTwo = "Height: 1.65m";
+let strThree = "Country: Nigeria";
+
+let strF =strOne +"<br>"+strTwo+"<br>" +strThree;
+document.write(strF);
